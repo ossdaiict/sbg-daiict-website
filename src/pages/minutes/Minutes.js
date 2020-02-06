c2b62e48fd59a7fa86250687a625dc979bad735c
Srikumar Sastry <srikumarsastry1997@gmail.com>
2020-02-06 13:24:43 +0530

    Updated MOM

--- src/pages/minutes/Minutes.js
+++ src/pages/minutes/Minutes.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { PageHeader, Panel } from "react-bootstrap";
-import { FaCheck, FaClock } from "react-icons/fa";
+import React from 'react'
+import { PageHeader, Panel } from 'react-bootstrap'
+import { FaCheck, FaClock } from 'react-icons/fa'
 
 class Minutes extends React.Component {
   render() {
@@ -21,6 +21,34 @@ class Minutes extends React.Component {
           <Panel.Body>
             <div>
               <ul className="timeline">
+                <li className="timeline-inverted">
+                  <div className="timeline-badge danger">
+                    <FaCheck />
+                  </div>
+                  <div className="timeline-panel">
+                    <div className="timeline-heading">
+                      <h4 className="timeline-title">
+                        SBG General Meet 1 (2020)
+                      </h4>
+                      <p>
+                        <small className="text-muted">
+                          <FaClock /> 04th February, 2020
+                        </small>
+                      </p>
+                    </div>
+                    <div className="timeline-body">
+                      <a
+                        target={'_blank'}
+                        rel={'noopener norefferer'}
+                        href={
+                          'https://drive.google.com/file/d/1Mqs_x22KebK9Wvrd3GG2nJZbImxNo6SP/view?usp=sharing'
+                        }
+                      >
+                        Click to view the minutes
+                      </a>
+                    </div>
+                  </div>
+                </li>
                 <li>
                   <div className="timeline-badge success">
                     <FaCheck />
@@ -38,10 +66,10 @@ class Minutes extends React.Component {
                     </div>
                     <div className="timeline-body">
                       <a
-                        target={"_blank"}
-                        rel={"noopener norefferer"}
+                        target={'_blank'}
+                        rel={'noopener norefferer'}
                         href={
-                          "https://docs.google.com/document/d/1dnZZpukHTwIMxsyYRd04_HJPysc72d1Zpk-EBKemwVw/edit?usp=sharing"
+                          'https://docs.google.com/document/d/1dnZZpukHTwIMxsyYRd04_HJPysc72d1Zpk-EBKemwVw/edit?usp=sharing'
                         }
                       >
                         Click to view the minutes
@@ -66,10 +94,10 @@ class Minutes extends React.Component {
                     </div>
                     <div className="timeline-body">
                       <a
-                        target={"_blank"}
-                        rel={"noopener norefferer"}
+                        target={'_blank'}
+                        rel={'noopener norefferer'}
                         href={
-                          "https://drive.google.com/open?id=1mmSIBA_-tXB4cucQwvh9JBab_nmOh4ExR_vPXMTTte8"
+                          'https://drive.google.com/open?id=1mmSIBA_-tXB4cucQwvh9JBab_nmOh4ExR_vPXMTTte8'
                         }
                       >
                         Click to view the minutes
@@ -94,10 +122,10 @@ class Minutes extends React.Component {
                     </div>
                     <div className="timeline-body">
                       <a
-                        target={"_blank"}
-                        rel={"noopener norefferer"}
+                        target={'_blank'}
+                        rel={'noopener norefferer'}
                         href={
-                          "https://docs.google.com/document/d/1dyjhdQIXdi9QVgYq5oHUf4PAxaDkx5gtM1vRWWvbvmE"
+                          'https://docs.google.com/document/d/1dyjhdQIXdi9QVgYq5oHUf4PAxaDkx5gtM1vRWWvbvmE'
                         }
                       >
                         Click to view the minutes
@@ -110,8 +138,8 @@ class Minutes extends React.Component {
           </Panel.Body>
         </Panel>
       </div>
-    );
+    )
   }
 }
 
-export default Minutes;
+export default Minutes
